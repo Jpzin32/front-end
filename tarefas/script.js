ba60d42f7de95cf61a971c5aa85286e6e4c9099f
Jonatha da Silva Pedrozas <158524181+Jpzin32@users.noreply.github.com>
2025-03-31 20:48:33 -0300

    Add files via upload

--- tarefas/script.js
+++ tarefas/script.js
@@ -1,3 +1,31 @@
-const task = document.getElementById('task')
+function createCloseButton(li) {
+    let span = document.createElement ("SPAN"); //inserir palavras em ordem
+    let txt = document.createTextNode("\u00D7"); //inserir as palavras como texto
 
-document.write(task)
+
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+span.onclick = () => span.parentElement.style.display = "none";
+}
+
+document.querySelectorAll('li').forEach(createCloseButton);
+
+document.querySelector('ul').addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI')
+    e.target.classList.toggle('checked');
+});
+
+function add() {
+    let li = document.createElement('LI');
+    let input_value = document.form_main.task.value;
+    let input_text = document.createTextNode(input_value);
+    
+
+    li.appendChild(input_text);
+    document.querySelector('ul').appendChild(li);
+    document.form_main.task.value = "";
+
+    createCloseButton(li);
+}
